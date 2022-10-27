db723ba96a93b30c05d59b7fbbee4c9c6e5fea18
Mauricio Lopes <mlopeesz@gmail.com>
2022-10-27 16:34:57 -0300

    feat: :sparkles: \class TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,19 +1,46 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+
+    def __init__(self):
+        self.data = []
+
     def __len__(self):
-        pass
+        return len(self.data)
+
+    def get_data_by_person(self, customer):
+        return [item for item in self.data if item[0] == customer]
 
     def add_new_order(self, customer, order, day):
-        pass
+        return self.data.append((customer, order, day))
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        order_list = []
+        for customer, order, day in self.data:
+            if (customer == customer):
+                order_list.append(order)
+
+        return max(set(order_list), key=order_list.count)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        customer_list = []
+        all_orders = []
+
+        for pedidos in self.data:
+            all_orders.append(pedidos[1])
+
+            if pedidos[0] == customer:
+                customer_list.append(pedidos[1])
+
+        all_unique_orders = set(all_orders)
+        customer_unique_orders = set(customer_list)
+
+        return all_unique_orders.difference(customer_unique_orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        customer_data = self.get_data_by_person(customer)
+        days_data = set([item[2] for item in self.data])
+        days_customer = set([item[2] for item in customer_data])
+
+        return days_data.difference(days_customer)
 
     def get_busiest_day(self):
         pass
